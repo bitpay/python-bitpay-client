45e53d3882d150290acee258c8117cd8be6603bd
Antonio Buedo <sales-engineering@bitpay.com>
2022-03-14 10:26:33 +0100

    #v3.4.2203.1-rc - bugfixes (#10)
    Co-authored-by: Antonio Buedo <anbugal@gmail.com>

--- src/bitpay/client.py
+++ src/bitpay/client.py
@@ -2,7 +2,7 @@
 Class Client
 package Bitpay
 author  Antonio Buedo
-version 3.3.2203
+version 14.3.2203
 See bitpay.com/api for more information.
 """
 import os
@@ -81,7 +81,7 @@ class Client:
     * Class Client
     * @package Bitpay
     * @author  Antonio Buedo
-    * @version 3.3.2203
+    * @version 3.4.2203
     * See bitpay.com/api for more information.
     """
 
